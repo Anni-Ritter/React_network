d6fd3b31c8bf6297d3e59bcc9c838d33e7d24067
Anni-Ritter <afon.anastasia@mail.ru>
2023-04-25 19:33:07 +0300

    init react app

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import s from "./Dialogs.module.css";
+
+const Dialogs = (probs) =>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default Dialogs;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import style from './Profile.module.css';
+import MyPost from "./MyPosts/MyPost";
+
+const Profile = () =>{
+    return(
+    <div className={style.content}>
+        <div>
+            <img src="https://adrenaline.com.br/uploads/chamadas/genshin_impact1.jpg" width="700px" height="300px"></img>
+        </div>
+        <div>
+            avatar+description
+        </div>
+        <MyPost/>
+    </div>
+    )
+}
+export default Profile;

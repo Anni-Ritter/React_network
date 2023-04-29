2cf27dc2d29bda77d164674cff553f011f354df1
AnNi_RiTtEr <afon.anastasia@mail.ru>
2023-04-30 00:01:18 +0300

    Fix package and App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import s from './App.module.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from "./components/Dialogs/Dialogs";
+import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Setting from "./components/Setting/Setting";
 
 function App() {
-  return (
-   <div className={s.wrapper}>
-   <Header/>
-   <Navbar/>
-   <Profile/>
-   </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-content'>
+                    <Routes>
+                        <Route path={'/dialogs'} element={<Dialogs/>}/>
+                        <Route path={'/profile'} element={<Profile/>}/>
+                        <Route path={'/music'} element={<Music/>}/>
+                        <Route path={'/news'} element={<News/>}/>
+                        <Route path={'/setting'} element={<Setting/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;

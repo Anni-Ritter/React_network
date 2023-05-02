42182a25ad5ff554f394fc6536184f428acef4c1
Anni-Ritter <afon.anastasia@mail.ru>
2023-05-02 14:34:16 +0300

    commit for pull

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import s from './App.module.css';
+import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
 
-function App() {
-  return (
-   <div className={s.wrapper}>
-   <Header/>
-   <Navbar/>
-   {/*<Profile/>*/}
-   <Dialogs/>
-   </div>
-  );
+function App(probs) {
+    return (
+        <div className={'app-wrapper'}>
+            <Header/>
+            <Navbar/>
+            <div className={'app-wrapper-content'}>
+                <Dialogs/>
+                <Profile/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import s from "./Dialogs.module.css";
 
-const Dialogs = (probs) =>{
-    return(
+const Dialogs = (probs) => {
+    return (
         <div>
-
+            dialogs
         </div>
     )
 }
